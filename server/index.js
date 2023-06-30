2214854b77b6e13c59939feb2484d0cdb06c54fe
Abdulnasser <abodkar96@gmail.com>
2023-06-30 18:36:07 +0300

    updated-database-connection

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 
+import postRoutes from './Routes/postRoutes.js'
+import dalleRoutes from './Routes/dall-eRoutes.js'
+
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/post',postRoutes)
+app.use('/api/v2/dalle', dalleRoutes)
+
+
 app.get('/', async (req, res) => {
   res.send('Hello From DALL-E');
 
