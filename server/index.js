903c89ba461c5a63da1c62e81100b1b29cb5752e
Abdulnasser <abodkar96@gmail.com>
2023-06-30 15:53:40 +0300

    setting up backend server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.get('/', async (req, res) => {
+  res.send('Hello From DALL-E');
+
+});
+const start = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('server listen at port number 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
